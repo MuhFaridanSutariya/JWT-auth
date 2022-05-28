33b8f2f757286ad473735459273cbcf94aee234b
Muh.FaridanSutariya <88027268+MuhFaridanSutariya@users.noreply.github.com>
2022-05-28 21:31:36 +0800

    init

---
+++ djangoAPIapp/serializers.py
@@ -0,0 +1,115 @@
+from asyncore import read
+from dataclasses import field
+from itertools import product
+from lib2to3.pgen2.tokenize import TokenError
+from os import set_inheritable
+from unittest.util import _MAX_LENGTH
+from django.http import QueryDict
+from rest_framework import serializers
+from .models import Category, Book, Product, Cart
+from django.contrib.auth.models import User
+
+
+        
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    
+    email = serializers.EmailField(max_length=50, min_length=6)
+    username = serializers.CharField(max_length=50, min_length=6)
+    password = serializers.CharField(max_length=150, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'first_name', 'last_name', 'email', 'username', 'password')
+
+    def validate(self, args):
+        email = args.get('email', None)
+        username = args.get('username', None)
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError({'email':('Email already exists!')})
+        if User.objects.filter(username=username).exists():
+            raise serializers.ValidationError({'username':('Username already exists!')})
+
+        return super().validate(args)
+    
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+class CategorySerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        fields = (
+            'id',
+            'title'
+        )
+        model = Category 
+
+
+class BookSerializer(serializers.ModelSerializer):
+    created_by = serializers.ReadOnlyField(source='created_by.username', read_only=False)
+    class Meta:
+        fields = (
+            'id',
+            'title',
+            'category',
+            'author',
+            'isbn',
+            'pages',
+            'price',
+            'stock',
+            'description',
+            'imageUrl',
+            'created_by',
+            'status',
+            'date_created'
+        )
+        model = Book
+
+class ProductSerializer(serializers.ModelSerializer):
+    created_by = serializers.ReadOnlyField(source='created_by.username', read_only=False)
+    class Meta:
+        fields = (
+            'id',
+            'product_tag',
+            'name',
+            'category',
+            'price',
+            'stock',
+            'imageUrl',
+            'created_by',
+            'status',
+            'date_created'
+        )
+        model = Product
+
+
+class UserSerializer(serializers.ModelSerializer):
+    books = serializers.PrimaryKeyRelatedField(many=True, queryset=Book.objects.all())
+    products = serializers.PrimaryKeyRelatedField(many=True, queryset=Product.objects.all())
+
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'username',
+            'email',
+            'books',
+            'products',
+        )
+
+class CartUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+
+class CartSerializer(serializers.ModelSerializer):
+
+    cart_id = CartUserSerializer(read_only=True, many=False)
+    books = BookSerializer(read_only=True, many=True)
+    products = ProductSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Cart
+        fields = ('cart_id', 'created_at', 'books', 'products')
